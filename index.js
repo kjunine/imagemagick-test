94a696281f82602bb66a66b46719cedb069b4b38
Daniel Ku <kjunine@gmail.com>
2014-08-01 08:55:45 +0900

    Feature: Sequential Execution
    - Executes sequentially by file for performance reason.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var fs = require('fs'),
     path = require('path'),
     sprintf = require('sprintf-js').sprintf,
     Bluebird = require('bluebird'),
+    _ = require('lodash'),
     PhotoUtil = require('./photo');
 
 var traverse = function(directory, callback) {
@@ -58,25 +59,27 @@ var load = function(file) {
   var target = rename(file, '.targets');
   var thumbnail = rename(file, '.thumbnails');
 
-  PhotoUtil
+  return PhotoUtil
     .load(file, {
       workingDirectory: '.tmp',
       // format: 'jpg',
       withThumbnail: true,
-      thumbnailFormat: 'jpg'
+      // thumbnailFormat: 'jpg'
     })
     .then(function(result) {
-      console.log(sprintf('%10s -> %9s: %s', path.basename(file), 'target', info(result.target.info)));
-      console.log(sprintf('%10s -> %9s: %s', path.basename(file), 'thumbnail', info(result.thumbnail.info)));
-
       return Bluebird.all([
         result.cleaner,
+        sprintf('%10s -> %9s: %s', path.basename(file), 'target', info(result.target.info)),
+        sprintf('%10s -> %9s: %s', path.basename(file), 'thumbnail', info(result.thumbnail.info)),
         write(target, result.target.stream),
         write(thumbnail, result.thumbnail.stream)
       ]);
     })
-    .spread(function(cleaner) {
-      return cleaner.clean();
+    .spread(function(cleaner, targetOutput, thumbnailOutput) {
+      return cleaner.clean()
+        .then(function() {
+          return [targetOutput, thumbnailOutput];
+        });
     })
     .catch(function(err) {
       console.error(file, '-> err:', err);
@@ -87,4 +90,25 @@ traverse('.tmp', clean);
 traverse('.thumbnails', clean);
 traverse('.targets', clean);
 
-traverse('images', load);
+var promise = Bluebird.resolve();
+var targetOutputs = [];
+var thumbnailOutputs = [];
+
+traverse('images', function(file) {
+  promise = promise.then(function() {
+    return load(file)
+      .spread(function(targetOutput, thumbnailOutput) {
+        targetOutputs.push(targetOutput);
+        thumbnailOutputs.push(thumbnailOutput);
+      });
+  });
+});
+
+promise.then(function() {
+  _.each(targetOutputs, function(targetOutput) {
+    console.log(targetOutput);
+  });
+  _.each(thumbnailOutputs, function(thumbnailOutput) {
+    console.log(thumbnailOutput);
+  });
+});
